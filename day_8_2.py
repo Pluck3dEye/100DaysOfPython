d74f254422df9f83c7e11e65fc26bbc882d40920
Dung Nguyen <91055788+hanz2110@users.noreply.github.com>
2024-05-05 13:25:18 +0700

    change day_8_2

--- day_8_2.py
+++ day_8_2.py
@@ -5,20 +5,33 @@ import string
 alphabet = list(string.ascii_lowercase)
 
 
-def encrypt(message, shift):
-    encrypted_message = ""
-    for letter in message:
-        position = alphabet.index(letter)
-        encrypted_message += alphabet[(position + shift) % len(alphabet)]
-    return encrypted_message
+# def encrypt(message, shift):
+#     encrypted_message = ""
+#     for letter in message:
+#         position = alphabet.index(letter)
+#         encrypted_message += alphabet[(position + shift) % len(alphabet)]
+#     return encrypted_message
+#
+#
+# def decrypt(message, shift):
+#     decrypted_message = ""
+#     for letter in message:
+#         position = alphabet.index(letter)
+#         decrypted_message += alphabet[((position - shift) + len(alphabet)) % len(alphabet)]
+#     return decrypted_message
 
 
-def decrypt(message, shift):
-    decrypted_message = ""
-    for letter in message:
-        position = alphabet.index(letter)
-        decrypted_message += alphabet[((position - shift) + len(alphabet)) % len(alphabet)]
-    return decrypted_message
+def caesar(message_, shift_, mode_):
+    new_message = ""
+    if mode_ == "encrypt":
+        for letter in message_:
+            position = alphabet.index(letter)
+            new_message += alphabet[(position + shift_) % len(alphabet)]
+    if mode_ == "decrypt":
+        for letter in message_:
+            position = alphabet.index(letter)
+            new_message += alphabet[((position - shift_) + len(alphabet)) % len(alphabet)]
+    return new_message
 
 
 end_program = False
@@ -26,12 +39,8 @@ while not end_program:
     message = input("Type your message: ")
     shift = int(input("Type the shift: "))
     mode = input("Type encrypt or decrypt: ")
-    if mode == "encrypt":
-        encrypted_message = encrypt(message, shift)
-        print(encrypted_message)
-    else:
-        decrypted_message = decrypt(message, shift)
-        print(decrypted_message)
+    message = caesar(message, shift, mode)
+    print(message)
     print("Do you want to continue? ", end="")
     if input("y/n\n") == "y":
         end_program = False
